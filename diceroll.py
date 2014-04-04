ba892db2cb6127f7d958576f39ff9987e08a1b1e
ALIEA N <dolcevita.ALN@gmail.com>
2014-04-03 20:10:59 -0400

    add new feature asking how many dice they want

--- diceroll.py
+++ diceroll.py
@@ -2,10 +2,13 @@ import random
 
 import specialInput
 
-diceSides = specialInput.int_input("how many sides does your die have?")
+diceNumber = specialInput.int_input("how many dice do you want to have")
 
-result = random.randint(1,diceSides)
+diceSides = specialInput.int_input("how many sides do your dice have?")
+for x in range(diceNumber):
 
-print "after you have read the README file, here is the result:"
+    result = random.randint(1,diceSides)
 
-print result
+    print "after you have read the README file, here is the result:"
+
+    print result
